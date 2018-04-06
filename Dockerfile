a075f8257edc59729d50ff5703a53106e272d0e0
Maksym Tymchyk <reilag69@gmail.com>
2018-04-06 12:27:19 +0300

    Fucked updates

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:9.7.1-alpine
+FROM node:9.8.0-alpine
 
 ENV NX_HOST=localhost
 ENV NX_PORT=80
--- src/Server/Views/HomeComponent/ShareInfo.tsx
+++ src/Server/Views/HomeComponent/ShareInfo.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as i18n from "i18n";
-import {IDomainProps}  from '../../Utils/DomainManager';
+import {IDomainProps} from '../../Utils/DomainManager';
 
 export interface IShareProps {
     domain: IDomainProps;
@@ -12,13 +12,14 @@ export class ShareInfo extends React.Component<IShareProps, any> {
         const {domain} = this.props;
 
         return <section className="share">
-
-            <h2 className="share__title">{i18n.__('Нравиться? Расшарь проект!')}</h2>
-
-            <p className="share__desc">{i18n.__('А представьте, как это понравиться вашим друзьям, даже если они ничего не смыслят о ICO, блокчейне или криптовалюте. Им будет интересно просто почитать о проекте.')}</p>
-
+            <h2 className="share__title">
+                {i18n.__('Нравится?')}
+            </h2>
+            <p className="share__desc">
+                {i18n.__('А представьте, как это понравится вашим друзьям, даже если они ничего не смыслят о ICO, блокчейне или криптовалюте. Им будет интересно просто почитать о проекте.')}
+            </p>
             <div className="share__buttons">
-                <div id="ShareComponent" className="share__buttons-round"></div>
+                <div id="ShareComponent" className="share__buttons-round"/>
             </div>
         </section>
     }
