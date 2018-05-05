578f4898fb2b9afb6bcffc6d0f1b7f75349fb206
Maksym Tymchyk <m.tymchyk@HQ-WS25-M.local>
2018-05-05 15:25:10 +0300

    Optimize Images

--- src/Server/Views/HomeComponent/Advisers.tsx
+++ src/Server/Views/HomeComponent/Advisers.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import * as i18n from "i18n";
 import {Adviser} from './SmallElements/Adviser';
-import {Emojify} from '../../Utils/Emojify';
 
 export class Advisers extends React.Component<any, any> {
     render() {
@@ -16,10 +15,12 @@ export class Advisers extends React.Component<any, any> {
                     </div>
 
                     <div className="adviser-list">
-                        <Adviser
+                        <Advisercoinwizard.png
+                            eco-wooden-frame.png
+                            spry.png
                             name="Andrey Krishtal"
                             role={i18n.__("Смотрит в код")}
-                            imageSrc="andrey-krishtal.png"
+                            imageSrc="andrey-krishtal.jpg"
                             description={i18n.__("В мире хайпа среди ICO проектов, этот является самым технологически интересным, ибо нет пустых обещаний и кучи рассказов про невозможные имплементации технологий в проект. Однозначно, мой единственный фаворит в мире ICO!")}
                             socialLinks={{
                                 facebook: 'https://www.facebook.com/andrew.kryshtal'
@@ -29,7 +30,7 @@ export class Advisers extends React.Component<any, any> {
                         <Adviser
                             name="Max Krupyshev"
                             role={i18n.__("Считает деньги")}
-                            imageSrc="max-krupyshev.png"
+                            imageSrc="max-krupyshev.jpg"
                             description={i18n.__("Я, как один из ранних участников крипто индустрии, уверен что проект НаХату будет успешным. Это один из немногих примеров успешного применения Блокчейна, уступающий разве что проекту CryptoKitties.")}
                             socialLinks={{
                                 facebook: 'https://www.facebook.com/LogZz.kieff'
@@ -39,7 +40,7 @@ export class Advisers extends React.Component<any, any> {
                         <Adviser
                             name="Igor Pertsiya"
                             role={i18n.__("Контролирует затраты")}
-                            imageSrc="igor-pertsiya.png"
+                            imageSrc="igor-pertsiya.jpg"
                             description={i18n.__("Я считаю, что применение технологии блокчейна и организация ICO для покупки Хаты - самое правильное и органичное явление на рынке криптовалют за последнее время, гарантирую, что прослежу, чтобы каждый цент был потрачен на НаХату и бокалы для бара в хате.")}
                             socialLinks={{
                                 facebook: 'https://www.facebook.com/igor.pertsiya'
@@ -49,7 +50,7 @@ export class Advisers extends React.Component<any, any> {
                         <Adviser
                             name="Veroslava Novosilnaya"
                             role={i18n.__("Следит за путешествиями")}
-                            imageSrc="veroslava-novosilnaya.png"
+                            imageSrc="veroslava-novosilnaya.jpg"
                             description={i18n.__("Я предпочитаю жить в разных странах мира в сьемных Хатах, но я считаю, что у каждого хорошего человека в Украине должна быть своя Хата. Обязуюсь приложить максимум усилий, чтобы проект НаХату получился успешным.")}
                             socialLinks={{
                                 facebook: 'https://www.facebook.com/veroslava.novosilnaya'
@@ -59,7 +60,7 @@ export class Advisers extends React.Component<any, any> {
                         <Adviser
                             name="David Braun"
                             role={i18n.__("Следит за ремонтом")}
-                            imageSrc="david-braun.png"
+                            imageSrc="david-braun.jpg"
                             description={i18n.__("У меня было много хат, и больших и маленьких. Я делал ремонты с большими и дорогими подрядчиками, пробовал работать бригадами Равшанов и Джамшутов. Сделаю все, что могу, чтобы проект НаХату реализовался максимально эффективно.")}
                             socialLinks={{
                                 facebook: 'https://www.facebook.com/david.braun.50951'
